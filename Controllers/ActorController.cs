e81537669685e0c5f9f19fadd7b5b8cbc732dac3
Mohamed Zakaria <132154144+moziko00@users.noreply.github.com>
2024-08-12 00:46:08 +0300

    updates

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,19 @@
+﻿using ETickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETickets.Controllers
+{
+    public class ActorController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index(int id)
+        {
+            var actor = context.Actors
+                .Include(a => a.ActorMovies)
+                    .ThenInclude(am => am.Movie) // Include movies
+                .FirstOrDefault(a => a.Id == id);
+            return View(actor);
+        }
+    }
+}
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -21,34 +21,42 @@ namespace ETickets.Controllers
         {
             if (string.IsNullOrWhiteSpace(searchString))
             {
-                // Redirect to Index or handle as needed
+                
                 return RedirectToAction(nameof(Index));
             }
+
             searchString = searchString.ToLower();
 
             var movies = context.Movies
-                .Include(m => m.Cinema) // Eager load Cinema
-                .Include(m => m.Category) // Eager load Category
+                .Include(m => m.Cinema) 
+                .Include(m => m.Category) 
                 .Where(m => m.Name.ToLower().Contains(searchString))
                 .ToList();
-            return View("Index",movies);
+
+            if (movies.Count == 0)
+            {
+                
+                return View("NotFound"); 
+            }
+
+            return View("Index", movies);
         }
+
 
         public IActionResult Details(int id)
         {
-            // Fetch the movie including related entities
             var movie = context.Movies
-                .Include(m => m.Cinema) // Eager load Cinema
-                .Include(m => m.Category) // Eager load Category
-                .FirstOrDefault(m => m.Id == id); // Get the movie by ID
+                .Include(m => m.Cinema)
+                .Include(m => m.Category)
+                .Include(m => m.ActorMovies) 
+                    .ThenInclude(am => am.Actor) 
+                .FirstOrDefault(m => m.Id == id);
 
-            // Check if the movie was found
             if (movie == null)
             {
-                return NotFound(); // Return a 404 error if the movie was not found
+                return NotFound();
             }
 
-            // Pass the movie to the view
             return View(movie);
         }
 
